4b8a421079f195264c6145aa693764b2356ad3e9
María Benavente <3081639+merybenavente@users.noreply.github.com>
2025-11-15 20:31:17 -0700

    update list of the POR generators

--- src/core/generator_types.py
+++ src/core/generator_types.py
@@ -5,6 +5,5 @@ class GeneratorType(str, Enum):
     """Enumeration of available generator types."""
 
     NAIVE = "naive"
-    WIZARDLM = "wizardlm"
     TEMPLATER = "templater"
     RAG_LLM = "rag_llm"
